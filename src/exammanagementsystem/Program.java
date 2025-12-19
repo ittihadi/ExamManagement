e9c64422111035be2bd78f13cbfa060be539831d
bakthiananda <bakthianandah@student.unp.ac.id>
2025-12-19 17:26:23 +0700

    Add project files

---
+++ src/exammanagementsystem/Program.java
@@ -0,0 +1,12 @@
+package exammanagementsystem;
+
+/**
+ *
+ * @author ittihadi
+ * @author bakthiananda
+ */
+public class Program {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+}
