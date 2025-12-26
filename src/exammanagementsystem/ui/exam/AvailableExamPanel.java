8e1466f970eaa436e736c0fb8c36969258ace764
Hadi <rittihadi@gmail.com>
2025-12-26 08:13:46 +0700

    Add exam status to available exams

--- src/exammanagementsystem/ui/exam/AvailableExamPanel.java
+++ src/exammanagementsystem/ui/exam/AvailableExamPanel.java
@@ -8,12 +8,20 @@ package exammanagementsystem.ui.exam;
 
 import javax.swing.*;
 import java.awt.*;
+import java.time.Instant;
+
 import exammanagementsystem.dao.ExamDAO;
+import exammanagementsystem.dao.QuestionDAO;
+import exammanagementsystem.dao.ResultDAO;
+import exammanagementsystem.dao.ResultDAO.Result;
+
 import javax.swing.table.DefaultTableModel;
 
 public class AvailableExamPanel extends JPanel {
 
     private ExamDAO examDAO = new ExamDAO();
+    private ResultDAO resultDAO = new ResultDAO();
+    private QuestionDAO questionDAO = new QuestionDAO();
     private JTable table;
     private DefaultTableModel model;
 
@@ -21,7 +29,7 @@ public class AvailableExamPanel extends JPanel {
         setLayout(new BorderLayout(10, 10));
 
         model = new DefaultTableModel(
-                new String[] { "Exam ID", "Title", "Start", "End" }, 0);
+                new String[] { "Exam ID", "Title", "Start", "End", "Status", "Score" }, 0);
         table = new JTable(model);
 
         loadData(userId);
@@ -31,12 +39,41 @@ public class AvailableExamPanel extends JPanel {
 
     private void loadData(String userId) {
         try {
+            Instant now = Instant.now();
+
             for (ExamDAO.Exam e : examDAO.readByParticipant(userId)) {
+                String status = null;
+                Instant start_inst = e.getStartTime().toInstant();
+                Instant end_inst = e.getEndTime().toInstant();
+
+                if (now.isBefore(start_inst)) {
+                    status = "Upcoming";
+                } else if (now.isAfter(end_inst)) {
+                    status = "Passed";
+                } else {
+                    status = "Ongoing";
+                }
+
+                int questionNum = questionDAO.readByExam(e.getId()).size();
+                float scoreSum = 0;
+
+                java.util.List<Result> userResults = resultDAO.readByExamAndUser(e.getId(), userId);
+                for (Result result : userResults) {
+                    scoreSum += result.getScore();
+                }
+
+                float scoreAvg = 0;
+                if (questionNum > 0) {
+                    scoreAvg = scoreSum / questionNum;
+                }
+
                 model.addRow(new Object[] {
                         e.getId(),
                         e.getTitle(),
                         e.getStartTime(),
-                        e.getEndTime()
+                        e.getEndTime(),
+                        status,
+                        scoreAvg
                 });
             }
         } catch (Exception ex) {
